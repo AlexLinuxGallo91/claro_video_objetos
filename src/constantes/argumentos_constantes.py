74633a52e11e3116edb9ec99f125faab6143e57b
Alexis Araujo <Alex_91y@hotmail.com>
2021-04-29 20:03:45 -0500

    Se arreglan headers

--- src/constantes/argumentos_constantes.py
+++ src/constantes/argumentos_constantes.py
@@ -32,5 +32,5 @@ SUBJECT_SEQUENCES = 'Notificación de discontinuidad en Series en Super Destacad
 SUBJECT_IMAGES = 'Notificación de inconcistencia en imágenes del Super Destacado: {} de la región {}.'
 
 # HTML STYLEs
-HTML_STYLE_BORDER_TABLE = 'border: 1px dotted black; border-collapse: collapse;'
+HTML_STYLE_BORDER_TABLE = 'border: 1px dotted black; border-collapse: collapse; padding: 5px;'
 HTML_STYLE_HEADER = 'border: 1px dotted black; border-collapse: collapse; background: #DEEAF6;'
