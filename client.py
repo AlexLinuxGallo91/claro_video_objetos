68abb485e39defe61e4d10cf19bd0aa7dce19712
Alexis Araujo <Alex_91y@hotmail.com>
2021-04-29 19:12:12 -0500

    Se agregan fixes para el generado de subjects dinamicos

--- client.py
+++ client.py
@@ -9,13 +9,13 @@ from src.utils.json_utils import JsonUtils
 from src.utils.html_utils import HtmlUtils
 import src.constantes.argumentos_constantes as const
 
-
 gm_client = GearmanClient(['localhost:4770'])
 
 lista_constantes_imagenes = ['SUCCESS', 'FAILED']
 lista_de_jobs = []
 contador_errores = 0
 pais_por_buscar = sys.argv[1]
+lista_correos_destinatarios = ['alexis.araujo@triara.com', 'jose.hernandez@triara.com', 'gerardo.trevino@triara.com']
 
 # se establece una lista con los jobs a ejecutar, donde cada job contiene la region establecida y cada uno de los
 # nodos
@@ -32,11 +32,10 @@ json_error = {}
 json_error['error'] = []
 
 # DEBUG
-const.CONST_MODO_DEBUG = True
+const.CONST_MODO_DEBUG = False
 
 for completed_job_request in completed_requests:
         result = completed_job_request.result
-
         try:
             json_response_obtenido = json.loads(result)
             json_response_obtenido = JsonUtils.generar_json_result_base(json_response_obtenido)
@@ -47,28 +46,18 @@ for completed_job_request in completed_requests:
 json_result = {}
 json_result['response'] = lista_result_response
 json_result_texto = json.dumps(json_result, indent=4)
-json_result_texto = '<h3>{}</h3>'.format(json_result_texto)
 
 # print('{}\n'.format(json_result_texto))
 
 # valida si existen imagenes corruptas, en caso de ser asi se forma una tabla HTML para su notificacion por correo
 if JsonUtils.se_presentan_urls_imagenes_corruptas(json_result):
-
-    # se forma la tabla
     HTML = HtmlUtils.generar_html_table_errores_imagenes(json_result)
-
-    # envio de correos
-    resp = MailUtils.enviar_correo(['alexis.araujo@triara.com'], 'notificacion.itoc@triara.com',
-                                   'prueba', HTML)
-
+    subject = MailUtils.subject_imagenes_dinamico(json_result)
+    resp = MailUtils.enviar_correo(lista_correos_destinatarios, 'notificacion.itoc@triara.com',subject, HTML)
     print(resp.text)
 
-if JsonUtils.se_presentan_urls_imagenes_corruptas(json_result):
-    # se forma la tabla
+if JsonUtils.se_presentan_secuencias_corruptas(json_result):
+    subject = MailUtils.subject_sequences_dinamico(json_result)
     HTML = HtmlUtils.generar_html_table_errores_secuencias(json_result)
-
-    # envio de correos
-    resp = MailUtils.enviar_correo(['alexis.araujo@triara.com'], 'notificacion.itoc@triara.com',
-                                   'prueba', HTML)
-
+    resp = MailUtils.enviar_correo(lista_correos_destinatarios, 'notificacion.itoc@triara.com', subject, HTML)
     print(resp.text)
--- src/utils/mail_utils.py
+++ src/utils/mail_utils.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Dict
 import requests
+import src.constantes.argumentos_constantes as const
 
 class MailUtils:
 
@@ -16,3 +17,38 @@ class MailUtils:
         response = requests.post(url_api_correo, data=data)
 
         return response
+
+    @staticmethod
+    def subject_sequences_dinamico(json_result_sequences:Dict):
+        region = ''
+        lista_nodos_afectados = []
+
+        for json_nodo in json_result_sequences['response']:
+            region = json_nodo['region']
+            if json_nodo['status'] != const.CONST_SUCCESS:
+                for json_error in json_nodo['errors']:
+                    if 'images' in json_error:
+                        lista_nodos_afectados.append(json_nodo['superhighlight'])
+                        break
+
+        lista_nodos_afectados = list(dict.fromkeys(lista_nodos_afectados))
+
+        return const.SUBJECT_SEQUENCES.format(', '.join(lista_nodos_afectados), region)
+
+    @staticmethod
+    def subject_imagenes_dinamico(json_result_sequences: Dict):
+        region = ''
+        lista_nodos_afectados = []
+
+        for json_nodo in json_result_sequences['response']:
+            region = json_nodo['region']
+            if json_nodo['status'] != const.CONST_SUCCESS:
+                for json_error in json_nodo['errors']:
+                    if 'sequences' in json_error:
+                        lista_nodos_afectados.append(json_nodo['superhighlight'])
+                        break
+
+        lista_nodos_afectados = list(dict.fromkeys(lista_nodos_afectados))
+
+        return const.SUBJECT_SEQUENCES.format(', '.join(lista_nodos_afectados), region)
+
